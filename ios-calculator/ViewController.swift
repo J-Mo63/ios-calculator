f6b43cfd33940eb2a98e2ab2d70668447c6504be
Jonathan Moallem <jdmoallem@gmail.com>
2018-04-17 15:00:35 +1000

    Made fixes to layout

--- ios-calculator/ViewController.swift
+++ ios-calculator/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func contentButtonClicked(_ sender: UIButton) {
+        label.text = label.text! + sender.titleLabel!.text!
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        label.text = ""
+    }
+    
+    @IBAction func del(_ sender: UIButton) {
+        guard let text: String = label.text else {
+            label.text = "error"
+            return
+        }
+        label.text = String(text.dropLast())
+    }
+    
+    @IBAction func solve(_ sender: UIButton) {
+        let expr = NSExpression(format: label.text!)
+        
+        if let result = expr.expressionValue(with: nil, context: nil) as? NSNumber {
+            label.text = result.stringValue
+        }
+        else {
+            label.text = "error"
+        }
+    }
 }
 
